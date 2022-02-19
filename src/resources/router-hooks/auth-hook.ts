a887f7c1015a723c5334383aff110652dd905f3f
Felix Randle <felix@randle.org>
2022-02-19 19:25:43 +0000

    feat: Add in toasts

--- src/resources/router-hooks/auth-hook.ts
+++ src/resources/router-hooks/auth-hook.ts
@@ -1,10 +1,11 @@
 import { lifecycleHooks, Params, RouteNode, inject } from "aurelia";
 import { SessionService } from "../../services/session-service";
+import { ToastService } from "../../services/toast-service";
 
 @lifecycleHooks()
-@inject(SessionService)
+@inject(SessionService, ToastService)
 export class AuthHook {
-    constructor(readonly sessionService: SessionService) {
+    constructor(readonly sessionService: SessionService, readonly toast: ToastService) {
     }
 
     canLoad(viewModel, params: Params, next: RouteNode, current: RouteNode) {
@@ -16,6 +17,7 @@ export class AuthHook {
             return true;
         }
 
+        this.toast.error('You must be logged in to access that page!')
         return '';
     }
 }
---
+++ src/services/toast-service.ts
@@ -0,0 +1,41 @@
+import Toast from 'lets-toast';
+
+export class ToastService {
+    private _toast;
+    constructor() {
+        this._toast = Toast({
+            // Delay in miliseconds. Set to 0 for non disappearing toast notifications.
+            delay: 5000,
+
+            // Dismiss button text. Only shown if `dismissible` is `true`.
+            dismiss: 'dismiss',
+
+            // Toast notifications are dismissible.
+            dismissible: true,
+
+            // Place new toast notifications at the top of the stack.
+            // If `vPos` is set to 'bottom', new toast notifications will be placed on
+            // the bottom of the stack.
+            newestAtTop: true,
+
+            // Toast notification severity (info, error, success, warning).
+            severity: 'info',
+
+            // Horizontal screen positioning (left, center, right).
+            hPos: 'right',
+
+            // Vertical screen positioning (top, center, bottom).
+            vPos: 'top',
+        })
+    }
+
+    info(content: string) {
+        this._toast(content);
+    }
+
+    error(content: string) {
+        this._toast(content, {
+            severity: 'warning'
+        })
+    }
+}
