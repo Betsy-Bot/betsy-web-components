cb5fc024bf32823c1323a255d0c53cfa40292d8b
Collin Herber <rustaddicts@gmail.com>
2022-02-18 20:15:02 -0500

    chore: update server dashboard styling

--- src/resources/elements/server-card/server-card.ts
+++ src/resources/elements/server-card/server-card.ts
@@ -2,4 +2,11 @@ import {bindable} from "aurelia";
 
 export class ServerCard {
     @bindable() server;
+
+    getServerText() {
+        const matches = this.server.name.match(/\b(\w)/g);
+        const acronym = matches.join('').toString();
+        console.log(acronym);
+        return acronym.length > 4 ? acronym.slice(0, 4) : acronym;
+    }
 }
