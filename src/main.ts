a8f56b832db3271cdfb7a3013965761d9364684e
Collin Herber <rustaddicts@gmail.com>
2022-02-18 19:29:56 -0500

    chore: rename folder

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import Aurelia, { RouterConfiguration } from 'aurelia';
 import { AllConfiguration } from '@aurelia-mdc-web/all';
 import { App } from './app';
 
-import { Navigation } from "./resources/elements/navbar/navigation";
+import { Navigation } from "./resources/elements/navigation/navigation";
 import {ServerCard} from "./resources/elements/server-card/server-card";
 
 import 'bootstrap';
