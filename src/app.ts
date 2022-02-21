479bb23edca5cf7edf4726890b1153be7b9a6a91
Felix Randle <felix@randle.org>
2022-02-21 23:59:57 +0000

    feat: Move guild stuff to child routing

--- src/app.ts
+++ src/app.ts
@@ -18,16 +18,6 @@ import { SessionService } from "./services/session-service";
             path: 'guild/:guildId',
             component: import('./pages/guild/guild'),
             title: 'Guild',
-        },
-        {
-            path: 'guild/:guildId/response-message',
-            component: import('./pages/guild/response-message/response-message'),
-            title: 'Guild Response Messages',
-        },
-        {
-            path: 'guild/:guildId/response-message/create',
-            component: import('./pages/guild/response-message/create-response-message/create-response-message'),
-            title: 'Guild Response Messages',
         }
     ]
 })
--- src/pages/guild/guild.ts
+++ src/pages/guild/guild.ts
@@ -1,5 +1,20 @@
-import {inject, IRouteViewModel, Params, RouteNode, IEventAggregator} from "aurelia";
+import {inject, IRouteViewModel, Params, RouteNode, IEventAggregator, route} from "aurelia";
 
+// @ts-ignore
+@route({
+    routes: [
+        {
+            path: 'response-message',
+            component: import('./response-message/response-message'),
+            title: 'Guild Response Messages',
+        },
+        {
+            path: 'response-message/create',
+            component: import('./response-message/create-response-message/create-response-message'),
+            title: 'Guild Response Messages',
+        }
+    ]
+})
 @inject(IEventAggregator)
 export class Guild implements IRouteViewModel {
     constructor(private eventAggregator: IEventAggregator) {
