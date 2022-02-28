d06e30a4fa27eb048a8ad890b8cb4719dbe20f81
Collin Herber <rustaddicts@gmail.com>
2022-02-28 16:33:18 -0500

    feat: add capability for multiple embeds per message

--- src/resources/elements/discord-message-creator/discord-message-creator.ts
+++ src/resources/elements/discord-message-creator/discord-message-creator.ts
@@ -1,15 +1,29 @@
 import {bindable} from 'aurelia';
+import {DiscordEmbed} from "../../../services/models/discord";
 
 export class DiscordMessageCreator {
     @bindable message;
-    activeTab: string = 'message';
-
 
     bound() {
-       console.log('message', this.message);
+        if (this.message.embeds?.length > 0) {
+            this.message.type = "embed"
+        } else {
+            this.message.type = "message";
+        }
+    }
+
+    addEmbed() {
+        if (!this.message.embeds) {
+            this.message.embeds = [];
+        }
+        this.message.embeds.push(new DiscordEmbed())
+    }
+
+    deleteEmbed(index) {
+        this.message.embeds.splice(index, 1)
     }
 
     selectTab(tab) {
-        this.activeTab = tab;
+        this.message.type = tab;
     }
 }
