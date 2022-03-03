02cec228047dc839535025e29f8ba18d3c800c84
Collin Herber <rustaddicts@gmail.com>
2022-03-02 21:09:18 -0500

    chore: remove color picker temporarily

--- src/pages/guild/guild.ts
+++ src/pages/guild/guild.ts
@@ -1,5 +1,5 @@
 import { routes, IRouter } from 'aurelia-direct-router';
-import {inject, IRouteViewModel, IEventAggregator, customElement, RouteNode, Params} from "aurelia";
+import {inject, IRouteViewModel, IEventAggregator, RouteNode, Params} from "aurelia";
 
 @routes([
     {
