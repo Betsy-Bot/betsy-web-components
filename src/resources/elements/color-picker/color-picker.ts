da8d9760c6e31c50e7456c2d2166208b3b24aced
Collin Herber <rustaddicts@gmail.com>
2022-03-09 13:30:52 -0500

    feat: add color picker back for embeds

--- src/resources/elements/color-picker/color-picker.ts
+++ src/resources/elements/color-picker/color-picker.ts
@@ -3,4 +3,14 @@ import {bindable} from 'aurelia';
 export class ColorPicker {
     @bindable label;
     @bindable value;
+    color;
+
+    bound() {
+        this.color = `#${this.value.toString(16)}`;
+    }
+
+    getDecimalValue() {
+        let replaced = this.color.replace('#', '');
+        this.value = parseInt(replaced, 16);
+    }
 }
