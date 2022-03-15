2c1ffd14dba11cd35052a91b4a614c1b85f805c7
Collin Herber <rustaddicts@gmail.com>
2022-03-15 15:48:51 -0400

    feat: add markup rendering for embed preview

--- src/resources/elements/discord-embed-preview/discord-embed-preview.ts
+++ src/resources/elements/discord-embed-preview/discord-embed-preview.ts
@@ -1,6 +1,16 @@
 import {bindable} from "aurelia-framework";
+import * as showdown from 'showdown';
+import sanitizeHtml from 'sanitize-html';
+
 import './discord-embed-preview.scss';
 
 export class DiscordEmbedPreview {
     @bindable embed;
+
+    getMarkup(content) {
+        const converter = new showdown.Converter({ strikethrough: true });
+        content.replace(/(\r\n|\r|\n)/g, '<br>');
+        let html = converter.makeHtml(content);
+        return sanitizeHtml(html);
+    }
 }
