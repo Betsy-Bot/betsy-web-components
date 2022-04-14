23092a407ecc87afbf355706bd5eced94bb86160
Collin Herber <rustaddicts@gmail.com>
2022-04-14 14:30:11 -0400

    feat: add global back button

--- src/resources/elements/discord-feature-header/discord-feature-header.ts
+++ src/resources/elements/discord-feature-header/discord-feature-header.ts
@@ -1,6 +1,15 @@
-import {bindable} from "aurelia-framework";
+import {bindable, inject} from "aurelia-framework";
+import {Router} from "aurelia-router";
 
+@inject(Router)
 export class DiscordFeatureHeader {
+    constructor(private router: Router) {
+    }
     @bindable title;
     @bindable subtitle;
+
+
+    navigateBack() {
+        this.router.navigateBack();
+    }
 }
