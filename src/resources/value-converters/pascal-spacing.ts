18d76083b65813ad4912d13157235aa0c6ebb60b
Collin Herber <rustaddicts@gmail.com>
2022-04-22 16:47:56 -0400

    feat: add some buttons to navigation

---
+++ src/resources/value-converters/pascal-spacing.ts
@@ -0,0 +1,10 @@
+export class PascalSpacingValueConverter  {
+    toView(value) {
+        if (!value) {
+            return;
+        }
+
+        value = value.replace(/([A-Z])/g, ' $1').trim();
+        return value;
+    }
+}
