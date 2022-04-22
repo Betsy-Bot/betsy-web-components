319440e54b36c9a23726b1cc67ab7cf0bb20481e
Collin Herber <rustaddicts@gmail.com>
2022-04-22 16:47:34 -0400

    chore: add some basic stats to the dashboard page

--- src/pages/guild/dashboard/dashboard.ts
+++ src/pages/guild/dashboard/dashboard.ts
@@ -1,3 +1,56 @@
-export class Dashboard {
+import {DiscordService} from "services/discord-service";
+import './dashboard.scss';
+import {inject} from "aurelia-framework";
+import {get} from "https";
 
+@inject(DiscordService)
+export class Dashboard {
+    constructor(private discordService: DiscordService) {
+    }
+
+    guildId;
+    guild;
+    channels;
+
+    stats = [
+        {
+            prefix: '~',
+            title: 'Member Count',
+            property: 'approximateMemberCount'
+        },
+        {
+            title: 'Emoji Count',
+            function: 'getEmojiCount'
+        },
+        {
+            title: 'Channel Count',
+            function: 'getEmojiCount'
+        }
+    ]
+
+    async activate(params) {
+        this.guildId = params.guildId as string;
+    }
+
+    async attached() {
+        [this.guild, this.channels] = await Promise.all([
+            await this.discordService.getDiscordServerInformation(this.guildId),
+            await this.discordService.getDiscordChannels(this.guildId)
+        ])
+    }
+
+    getStat(stat) {
+        if (stat.property) {
+            return this.guild.guild[stat.property];
+        }
+        return this[stat.function]();
+    }
+
+    getEmojiCount() {
+        return this.guild.guild.emojis.length
+    }
+
+    getChannelCount() {
+        return this.channels.length
+    }
 }
