0abf56e283c989b8b487c79f8efff27a90dbc9d0
Collin Herber <rustaddicts@gmail.com>
2022-08-14 13:30:20 -0500

    feat: add closed by and mentions replacing

--- src/resources/elements/support-ticket-transcript/support-ticket-transcript.ts
+++ src/resources/elements/support-ticket-transcript/support-ticket-transcript.ts
@@ -6,14 +6,23 @@ export class SupportTicketTranscript {
 
     attached() {
         this.generateParticipants();
+        this.replaceMentioned();
     }
 
     generateParticipants() {
-        for (let message of this.supportTicket.transcript.messages) {
+        for (const message of this.supportTicket.transcript.messages) {
             if (message.discordUserId != "943260131599220856" && message.discordUserId != "946433944390340688") {
                 if (!this.participants.includes(message.discordUserName)) {
-                    this.participants.push(message.discordUserName);
+                    this.participants.push({name: message.discordUserName, id: message.discordUserId});
                 }
+            }
+        }
+    }
+    
+    replaceMentioned() {
+        for (const participant of this.participants) {
+            for (const message of this.supportTicket.transcript.messages) {
+                message.message = message.message.replace(`<@${participant.id}>`, `@${participant.name}`)
             }
         }
     }
