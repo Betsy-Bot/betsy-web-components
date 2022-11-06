0c9cead7905cc63c5bcfd0d5431aff06b8e602d5
Collin Herber <rustaddicts@gmail.com>
2022-11-06 15:21:47 -0500

    feat: add ability to display warning if feature is off

--- src/resources/elements/discord-feature-header/discord-feature-header.ts
+++ src/resources/elements/discord-feature-header/discord-feature-header.ts
@@ -1,5 +1,6 @@
 import {bindable, inject} from "aurelia-framework";
 import {Router} from "aurelia-router";
+import './discord-feature-header.scss';
 
 @inject(Router)
 export class DiscordFeatureHeader {
@@ -10,8 +11,23 @@ export class DiscordFeatureHeader {
     @bindable subtitle;
     @bindable hideBack;
     @bindable docUrl;
+    @bindable displayWarning;
+    banner;
 
     navigateBack() {
         this.router.navigateBack();
+    }
+
+    attached() {
+        console.log(this.displayWarning)
+        if (this.displayWarning) {
+            setTimeout(() => {
+                this.banner.open();
+            })
+        } else {
+            setTimeout(() => {
+                this.banner.close();
+            })
+        }
     }
 }
