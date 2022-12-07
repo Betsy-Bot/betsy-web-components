487a58f3a4fa2cf362332bded4c8638dc81104de
CollinHerber <rustaddicts@gmail.com>
2022-12-06 20:53:48 -0500

    feat: add ability to clear emojis and form selects

--- src/resources/elements/discord-form-selector/discord-form-selector.ts
+++ src/resources/elements/discord-form-selector/discord-form-selector.ts
@@ -1,17 +1,26 @@
 import {inject, bindable} from "aurelia-framework";
 import {DiscordService} from "../../../services/discord-service";
+import {EventAggregator} from "aurelia-event-aggregator";
 
-@inject(DiscordService)
+@inject(DiscordService, EventAggregator)
 export class DiscordFormSelector {
-    constructor(private discordService: DiscordService) {
+    constructor(private discordService: DiscordService, private eventAggregator: EventAggregator) {
     }
     @bindable formId: string;
     @bindable label;
+    @bindable required;
     guildId: string;
     forms;
 
     async attached() {
         this.guildId = await this.discordService.getLocalDiscordGuildId();
         this.forms = await this.discordService.getDiscordForms(this.guildId);
+    }
+
+    //Temp solution because it wasn't clearing it for some reason
+    formIdChanged() {
+        if (!this.formId) {
+            this.eventAggregator.publish('form-cleared');
+        }
     }
 }
