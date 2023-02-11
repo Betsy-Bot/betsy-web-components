1286991493dfdb28235e2c63029c6d40f6af17c8
CollinHerber <rustaddicts@gmail.com>
2023-02-11 10:09:22 -0500

    wip: key value storage

---
+++ src/pages/guild/resources/key-value-storage/key-value-storage.ts
@@ -0,0 +1,23 @@
+import {Router} from 'aurelia-router';
+import {inject} from "aurelia-framework";
+import {EventAggregator} from "aurelia-event-aggregator";
+import {DiscordService} from 'services/discord-service';
+
+@inject(EventAggregator, DiscordService, Router)
+export class KeyValueStorage {
+    constructor(private eventAggregator: EventAggregator, private discordService: DiscordService, private router: Router) {
+    }
+    guildId: string;
+    params;
+    items = [];
+
+    activate(params) {
+        this.params = params;
+        this.guildId = this.params.guildId;
+    }
+
+    goToItem(item) {
+        this.router.navigate(`/guild/${this.guildId}/resources/key-value-storage/${item.id}`)
+    }
+
+}
