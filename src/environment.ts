aecfe37b6f90abfb55994e5e35393e018fb35b04
CollinHerber <rustaddicts@gmail.com>
2023-04-12 09:40:06 -0400

    fix: add environment type fetching

--- src/environment.ts
+++ src/environment.ts
@@ -1,17 +1,29 @@
 function redirectUrl() {
-    return import.meta.env.VITE_REDIRECT_URL;
+    if (typeof process === 'undefined') {
+        return import.meta.env.VITE_REDIRECT_URL;
+    }
+    return process.env.VITE_REDIRECT_URL;
 }
 
 function verifyRedirectUrl() {
-    return import.meta.env.VITE_VERIFY_REDIRECT_URL;
+    if (typeof process === 'undefined') {
+        return import.meta.env.VITE_VERIFY_REDIRECT_URL;
+    }
+    return process.env.VITE_VERIFY_REDIRECT_URL;
 }
 
 function botClientId() {
-    return import.meta.env.VITE_BOT_CLIENT_ID;
+    if (typeof process === 'undefined') {
+        return import.meta.env.VITE_BOT_CLIENT_ID;
+    }
+    return process.env.VITE_BOT_CLIENT_ID;
 }
 
 function apiEndpoint() {
-    return import.meta.env.VITE_API_ENDPOINT;
+    if (typeof process === 'undefined') {
+        return import.meta.env.VITE_API_ENDPOINT;
+    }
+    return process.env.VITE_API_ENDPOINT;
 }
 
 export default this;
