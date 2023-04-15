b55f373c79739a412069221e6cb14331a7a31923
Collin Herber <rustaddicts@gmail.com>
2023-04-15 19:46:25 -0400

    chore: upload favicons

--- src/resources/elements/navigation/navigation.ts
+++ src/resources/elements/navigation/navigation.ts
@@ -1,28 +1,39 @@
-import {bindable, customElement, ICustomElementViewModel, inject} from 'aurelia';
+import {
+    bindable,
+    customElement,
+    ICustomElementViewModel,
+    inject,
+} from "aurelia";
 import { SessionService } from "../../../services/session-service";
-import { Router } from '@aurelia/router-lite';
-import { IEventAggregator } from 'aurelia';
+import { Router } from "@aurelia/router-lite";
+import { IEventAggregator } from "aurelia";
 import { redirectUrl, botClientId } from "../../../environment";
-import template from './navigation.html';
-import './navigation.scss';
+import template from "./navigation.html";
+import "./navigation.scss";
+
+import logo from "../../../images/logo.png";
 
 @customElement({
-    name: 'navigation',
+    name: "navigation",
     template: template,
-    containerless: true
+    containerless: true,
 })
 @inject(SessionService, Router, IEventAggregator)
 export class Navigation implements ICustomElementViewModel {
-    constructor(private sessionService: SessionService, private router: Router, readonly ea: IEventAggregator) {
-    }
+    constructor(
+        private sessionService: SessionService,
+        private router: Router,
+        readonly ea: IEventAggregator
+    ) {}
 
     @bindable() user: any;
     guildId: number;
     drawer = {
-        open: null
+        open: null,
     };
     currentRoute;
     donateDialog;
+    logo = logo;
 
     async handleServerChange(event: CustomEvent) {
         this.guildId = event?.detail?.value;
@@ -32,14 +43,17 @@ export class Navigation implements ICustomElementViewModel {
     }
 
     attached() {
-        this.drawer.open = this.sessionService.getStorageItem(SessionService.SIDEBAR_STATUS_KEY, true);
-        this.ea.subscribe('user-updated', payload => {
+        this.drawer.open = this.sessionService.getStorageItem(
+            SessionService.SIDEBAR_STATUS_KEY,
+            true
+        );
+        this.ea.subscribe("user-updated", (payload) => {
             this.user = payload;
             if (!this.user) {
                 this.guildId = null;
             }
         });
-        this.ea.subscribe('guild-updated', payload => {
+        this.ea.subscribe("guild-updated", (payload) => {
             this.guildId = payload as number;
         });
         //this.currentRoute = this.router.;
@@ -48,8 +62,11 @@ export class Navigation implements ICustomElementViewModel {
     async toggleSidebar() {
         const newDrawerStatus = !this.drawer.open;
         this.drawer.open = newDrawerStatus;
-        this.sessionService.saveStorageItem(SessionService.SIDEBAR_STATUS_KEY, String(newDrawerStatus));
-        this.ea.publish('drawer-updated', newDrawerStatus);
+        this.sessionService.saveStorageItem(
+            SessionService.SIDEBAR_STATUS_KEY,
+            String(newDrawerStatus)
+        );
+        this.ea.publish("drawer-updated", newDrawerStatus);
     }
 
     async logout() {
@@ -62,6 +79,6 @@ export class Navigation implements ICustomElementViewModel {
     }
 
     get avatarLink() {
-        return `https://cdn.discordapp.com/avatars/${this.user.discordId}/${this.user.avatar}.webp`
+        return `https://cdn.discordapp.com/avatars/${this.user.discordId}/${this.user.avatar}.webp`;
     }
 }
