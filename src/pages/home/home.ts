3d885efbd05df0ecd6e0a959bb2e7f4131dbd06e
CollinHerber <rustaddicts@gmail.com>
2023-04-23 08:52:27 -0400

    fix: server redirects not working on new invites

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,51 +1,52 @@
 import { inject } from 'aurelia';
-import { IRouteViewModel, Router } from '@aurelia/router-lite';
+import { IRouter,IRouteViewModel } from '@aurelia/router-lite';
 
 import { SessionService } from '../../services/session-service';
 import { UserService } from '../../services/user-service';
 import { WebhookService } from '../../services/websocket-service';
 
-@inject(SessionService, UserService, WebhookService, Router)
+@inject(SessionService, UserService, WebhookService, IRouter)
 export class Home implements IRouteViewModel {
-  constructor(private sessionService: SessionService, private userService: UserService, private webhookService: WebhookService, private router: Router) {}
-
-  user;
-  private managedGuilds;
-  private otherGuilds;
-  connection;
-
-  async attached() {
-    this.user = await this.sessionService.getUser();
-
-    if (this.user?.adminedServers) {
-      // eslint-disable-next-line no-unsafe-optional-chaining
-      for (const server of this.user?.adminedServers) {
-        server.id = server.guildId;
-      }
+    constructor(private sessionService: SessionService, private userService: UserService, private webhookService: WebhookService, private router: IRouter) {
     }
 
-    const userGuilds = await this.userService.getGuilds();
-    for (const guild of userGuilds) {
-      guild.icon_extension = guild.icon?.startsWith('a_') ? 'gif' : 'webp';
-      guild.can_add = guild.owner || (guild.permissions & 0x8) == 0x8;
+    user;
+    private managedGuilds;
+    private otherGuilds;
+    connection;
+
+    async attached() {
+        this.user = await this.sessionService.getUser();
+
+        if (this.user?.adminedServers) {
+            // eslint-disable-next-line no-unsafe-optional-chaining
+            for (const server of this.user?.adminedServers) {
+                server.id = server.guildId;
+            }
+        }
+
+        const userGuilds = await this.userService.getGuilds();
+        for (const guild of userGuilds) {
+            guild.icon_extension = guild.icon?.startsWith('a_') ? 'gif' : 'webp';
+            guild.can_add = guild.owner || (guild.permissions & 0x8) == 0x8;
+        }
+
+        this.managedGuilds = userGuilds.filter((g) => g.can_add);
+        this.otherGuilds = userGuilds.filter((g) => !g.can_add);
+
+        for (const guild of this.managedGuilds) {
+            const foundServerIndex = this.user.activeServers.findIndex((x) => x.guildId == guild.id);
+            if (foundServerIndex >= 0) {
+                guild.exists = true;
+                guild.invited = this.user.activeServers[foundServerIndex].invited;
+                this.user.activeServers[foundServerIndex].name = guild.name;
+            }
+        }
+
+        this.connection = this.webhookService.subscribeToGuildInvite();
+        await this.connection.start();
+        this.connection.on('BotInvited', async (id, name, description) => {
+            await this.router.load(`guild/${id}`);
+        });
     }
-
-    this.managedGuilds = userGuilds.filter((g) => g.can_add);
-    this.otherGuilds = userGuilds.filter((g) => !g.can_add);
-
-    for (const guild of this.managedGuilds) {
-      const foundServerIndex = this.user.activeServers.findIndex((x) => x.guildId == guild.id);
-      if (foundServerIndex >= 0) {
-        guild.exists = true;
-        guild.invited = this.user.activeServers[foundServerIndex].invited;
-        this.user.activeServers[foundServerIndex].name = guild.name;
-      }
-    }
-
-    this.connection = this.webhookService.subscribeToGuildInvite();
-    await this.connection.start();
-    this.connection.on('BotInvited', async (id, name, description) => {
-      await this.router.load(`/guild/${id}`);
-    });
-  }
 }
