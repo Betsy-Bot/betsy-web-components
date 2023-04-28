6eeb385468961c0d7c4457dd6274c6980b8dd1a3
Collin Herber <rustaddicts@gmail.com>
2023-04-28 16:40:56 -0400

    fix: redirect url for prod

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,42 @@
-import { IEventAggregator, inject } from 'aurelia';
-import { route, Router } from '@aurelia/router-lite';
+import { IEventAggregator, inject } from "aurelia";
+import { route, Router } from "@aurelia/router-lite";
 
 import { Transcript } from "./pages/transcript/transcript";
-import { SessionService } from './services/session-service';
+import { SessionService } from "./services/session-service";
 
-import './app.scss';
+import "./app.scss";
 
-import { toast } from 'lets-toast';
-import {Verify} from "./pages/verify/verify";
+import { toast } from "lets-toast";
+import { Verify } from "./pages/verify/verify";
 
 const routes = [
     {
-        path: '',
-        component: () => import('./pages/home/home'),
-        title: 'Home',
+        path: ["", "login"],
+        component: () => import("./pages/home/home"),
+        title: "Home",
     },
     {
-        path: 'guild/:guildId',
-        component: () => import('./pages/guild/guild'),
-        title: 'Guild',
+        path: "guild/:guildId",
+        component: () => import("./pages/guild/guild"),
+        title: "Guild",
     },
     {
-        path: '404',
-        component: () => import('./pages/four-oh-four/four-oh-four'),
-        title: '404',
+        path: "404",
+        component: () => import("./pages/four-oh-four/four-oh-four"),
+        title: "404",
     },
     Transcript,
-    Verify
+    Verify,
 ];
 
-@route({ title: 'Besty Bot', routes: routes, fallback: '404' })
+@route({ title: "Besty Bot", routes: routes, fallback: "404" })
 @inject(Router, IEventAggregator, SessionService)
 export class App {
-    constructor(private router: Router, private ea: IEventAggregator, private sessionService: SessionService) {}
+    constructor(
+        private router: Router,
+        private ea: IEventAggregator,
+        private sessionService: SessionService
+    ) {}
 
     user;
     guildId;
@@ -45,32 +49,36 @@ export class App {
         this.code = params.code;
         if (this.code) {
             try {
-                this.user = await this.sessionService.loginWithOAuthCode(this.code);
+                this.user = await this.sessionService.loginWithOAuthCode(
+                    this.code
+                );
             } catch (e) {
-                toast('Failed to exchange code', { severity: 'error' });
+                toast("Failed to exchange code", { severity: "error" });
             }
-            urlSearchParams.delete('code');
+            urlSearchParams.delete("code");
         } else {
             this.user = await this.sessionService.getUser();
         }
-        this.ea.subscribe('user-updated', (payload) => {
+        this.ea.subscribe("user-updated", (payload) => {
             this.user = payload;
         });
-        this.ea.subscribe('guild-updated', (payload) => {
+        this.ea.subscribe("guild-updated", (payload) => {
             this.guildId = payload;
         });
-        this.ea.subscribe('drawer-updated', (payload) => {
+        this.ea.subscribe("drawer-updated", (payload) => {
             this.drawerOpen = payload as boolean;
         });
 
         if (this.user) {
-            this.ea.publish('user-updated', this.user);
+            this.ea.publish("user-updated", this.user);
         }
 
         //For some reason without this timeout it fails to bind properly. Race condition
         setTimeout(() => {
             if (this.guildId && this.user) {
-                this.drawerOpen = this.sessionService.getStorageItem(SessionService.SIDEBAR_STATUS_KEY) as boolean;
+                this.drawerOpen = this.sessionService.getStorageItem(
+                    SessionService.SIDEBAR_STATUS_KEY
+                ) as boolean;
             }
         }, 100);
     }
