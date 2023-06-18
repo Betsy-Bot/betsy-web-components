af7aa65a8d056e4aee15894b6f7824e9388b7647
Collin Herber <rustaddicts@gmail.com>
2023-06-18 14:49:09 -0400

    feat: add ability to add prefix roles

--- src/resources/elements/support-ticket-settings/support-ticket-settings.ts
+++ src/resources/elements/support-ticket-settings/support-ticket-settings.ts
@@ -1,6 +1,9 @@
 import { bindable, containerless, inject } from "aurelia";
+import { watch } from "@aurelia/runtime-html";
 
 import { DiscordService } from "../../../services/discord-service";
+
+import { MDCDialog, MDCDialogCloseEvent } from "@material/dialog";
 
 export const SupportTicketAudits = [
     "Opened",
@@ -11,18 +14,25 @@ export const SupportTicketAudits = [
     "UserAdded",
     "UserRemoved",
 ];
+
 @containerless()
 @inject(DiscordService)
 export class SupportTicketSettings {
     constructor(private discordService: DiscordService) {}
+
     @bindable request;
-    @bindable guildId;
     @bindable authorizedRole;
     @bindable selectedAuditOption;
+    addRoleOptionDialog: MDCDialog;
+    roleToAdd = {
+        roleId: "",
+        prefixOverride: "",
+    };
 
     roles: any[];
     tab = "settings";
     options: string[] = SupportTicketAudits;
+
     async binding() {
         this.roles = await this.discordService.getDiscordRoles();
     }
@@ -45,11 +55,33 @@ export class SupportTicketSettings {
         this.request.assignedRoles.splice(index, 1);
     }
 
+    removeRoleOption(index) {
+        this.request.supportTicketRoleOptions.splice(index, 1);
+    }
+
     getRoleName(roleId) {
         const found = this.roles.find((x) => x.id == roleId);
         if (!found) {
             return "";
         }
         return found?.name;
+    }
+
+    @watch("roleToAdd.roleId")
+    roleToAddIdChanged() {
+        if (this.roleToAdd.roleId) {
+            this.addRoleOptionDialog.open();
+        }
+    }
+
+    addRoleOption(event: MDCDialogCloseEvent) {
+        if (event.detail.action == "ok") {
+            if (!this.request.supportTicketRoleOptions) {
+                this.request.supportTicketRoleOptions = [];
+            }
+            console.log(this.roleToAdd);
+            this.request.supportTicketRoleOptions.push(this.roleToAdd);
+            this.roleToAdd = { roleId: "", prefixOverride: "" };
+        }
     }
 }
