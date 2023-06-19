9f9fe3a2c8c718b16c73fa8f1813a6bbf8b3956a
Collin Herber <rustaddicts@gmail.com>
2023-06-19 12:47:55 -0400

    fix: previewing button style

--- src/resources/elements/discord-button/discord-button.ts
+++ src/resources/elements/discord-button/discord-button.ts
@@ -1,28 +1,32 @@
 import { bindable, containerless } from "aurelia";
 
-import './discord-button.scss';
+import "./discord-button.scss";
 
 @containerless()
 export class DiscordButton {
     @bindable button;
-    getButtonClass(style) {
+
+    getButtonClass(style: string | number) {
+        if (typeof(style) == 'string') {
+            style = parseInt(style);
+        }
         switch (style) {
-        case 1:
-            return 'discord-button-primary'
-        case 3:
-            return 'discord-button-success'
-        case 2:
-            return 'discord-button-secondary'
-        case 4:
-            return 'discord-button-danger'
-        case 5:
-            return 'discord-button-link'
+            case 1:
+                return "discord-button-primary";
+            case 3:
+                return "discord-button-success";
+            case 2:
+                return "discord-button-secondary";
+            case 4:
+                return "discord-button-danger";
+            case 5:
+                return "discord-button-link";
         }
     }
 
     get displayEmoji() {
         if (this.button.emoji.name) {
-            return this.button.emoji.name
+            return this.button.emoji.name;
         }
         return null;
     }
