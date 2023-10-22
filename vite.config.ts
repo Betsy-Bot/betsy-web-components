a5aed95cc5d41efb8a0e3346678246046de48ce2
CollinHerber <rustaddicts@gmail.com>
2023-10-22 10:40:02 -0400

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
-import {defineConfig} from "vite";
-import aurelia from "@aurelia/vite-plugin";
+import { defineConfig } from 'vite';
+import aurelia from '@aurelia/vite-plugin';
 import path from 'path';
 import typescript from '@rollup/plugin-typescript';
 import { typescriptPaths } from 'rollup-plugin-typescript-paths';
 import * as packageJson from './package.json';
 
 export default defineConfig({
+    plugins: [
+        aurelia(),
+    ],
     build: {
         manifest: true,
         minify: true,
         reportCompressedSize: true,
         lib: {
             entry: path.resolve(__dirname, 'src/index.ts'),
-            name: 'aurelia-ionic',
+            name: '@worm425/betsy-web-components',
             fileName: 'index',
             formats: ['es', 'cjs'],
         },
