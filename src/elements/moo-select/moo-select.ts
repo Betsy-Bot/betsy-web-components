336ba5eec741b057b26d1e3bf66fbd12a3f8fb6e
Collin Herber <rustaddicts@gmail.com>
2023-11-03 09:07:37 -0400

    fix: value binding on none-string values

--- src/elements/moo-select/moo-select.ts
+++ src/elements/moo-select/moo-select.ts
@@ -1,4 +1,4 @@
-import {bindable, BindingMode, customElement, ICustomElementViewModel} from "@aurelia/runtime-html";
+import { bindable, BindingMode, customElement, ICustomElementViewModel } from '@aurelia/runtime-html';
 
 export interface IMooSelectOption {
     label: string;
@@ -9,11 +9,11 @@ export type MooSelectType = 'filled' | 'outlined';
 
 import template from './moo-select.html?raw';
 
-@customElement({name: 'moo-select', template, containerless: true})
+@customElement({ name: 'moo-select', template, containerless: true })
 export class MooSelect implements ICustomElementViewModel {
     @bindable label: string;
     @bindable options: IMooSelectOption[];
-    @bindable({mode: BindingMode.twoWay}) value: string | number | undefined;
+    @bindable({ mode: BindingMode.twoWay }) value: string | number | undefined;
     @bindable class: string;
     @bindable required: boolean;
     @bindable readonly: boolean;
@@ -22,5 +22,15 @@ export class MooSelect implements ICustomElementViewModel {
 
     handleChange(e: Event) {
         this.value = (e.target as HTMLSelectElement).value;
+    }
+
+    shouldBeSelected(option: IMooSelectOption | null) {
+        if (!this.includeNull && !this.value) {
+            return true;
+        }
+        if (!option) {
+            return false
+        }
+        return option.value == this.value;
     }
 }
