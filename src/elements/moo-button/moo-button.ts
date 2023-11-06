d168714f18235baf81f9f558f02045e3e58b0de5
Collin Herber <rustaddicts@gmail.com>
2023-11-06 12:43:24 -0500

    refactor: change md-type to variant

--- src/elements/moo-button/moo-button.ts
+++ src/elements/moo-button/moo-button.ts
@@ -7,14 +7,14 @@ import '@material/web/button/outlined-button.js';
 import '@material/web/button/text-button.js';
 import '@material/web/button/filled-tonal-button.js';
 
-export type MOO_BUTTON_MD_TYPE = 'filled' | 'outlined' | 'text' | 'elevated';
+export type MOO_BUTTON_VARIANT = 'filled' | 'outlined' | 'text' | 'elevated';
 import template from './moo-button.html?raw';
 
 @customElement({ name: 'moo-button', template, containerless: true, capture: true })
 export class MooButton implements ICustomElementViewModel {
     button: HTMLButtonElement;
     @bindable label: string;
-    @bindable mdType: MOO_BUTTON_MD_TYPE = 'filled';
+    @bindable variant: MOO_BUTTON_VARIANT = 'filled';
     @bindable icon: string;
     @bindable click: () => void;
     @bindable dataMdcDialogAction;
