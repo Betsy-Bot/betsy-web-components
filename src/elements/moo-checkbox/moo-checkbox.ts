d2b73c9af66bafbe8f9f6934ecfe64a34fd80dee
Collin Herber <rustaddicts@gmail.com>
2023-11-11 09:25:23 -0500

    chore: update checkbox to material 3

--- src/elements/moo-checkbox/moo-checkbox.ts
+++ src/elements/moo-checkbox/moo-checkbox.ts
@@ -1,20 +1,17 @@
-import './moo-checkbox.scss';
-import { MDCCheckbox } from '@material/checkbox';
-import {bindable, BindingMode, customElement, ICustomElementViewModel} from "@aurelia/runtime-html";
+import { bindable, BindingMode, customElement, ICustomElementViewModel } from '@aurelia/runtime-html';
 
 import template from './moo-checkbox.html?raw';
-@customElement({name:'moo-checkbox', template, containerless: true})
+
+import './moo-checkbox.scss';
+
+@customElement({ name: 'moo-checkbox', template, containerless: true, capture: true })
 export class MooCheckbox implements ICustomElementViewModel {
-    checkboxEl: HTMLElement;
     @bindable({ mode: BindingMode.twoWay }) checked: boolean;
-    @bindable changed;
+    @bindable changed: (value: string, checked: boolean) => void;
     @bindable({ mode: BindingMode.twoWay }) value: string;
 
-    attached() {
-        new MDCCheckbox(this.checkboxEl);
-    }
-
-    checkedChanged() {
+    handleChange() {
+        this.checked = !this.checked;
         if (this.changed) {
             this.changed(this.value, this.checked);
         }
