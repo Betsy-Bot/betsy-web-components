85d3d1f2a72c8c3153315fb11392c7214d289d11
Collin Herber <rustaddicts@gmail.com>
2023-11-12 14:57:41 -0500

    fix: incorrect export for text field

--- src/elements/moo-text-field/moo-text-field.ts
+++ src/elements/moo-text-field/moo-text-field.ts
@@ -4,7 +4,7 @@ import template from './moo-text-field.html?raw';
 
 export type MOO_TEXT_FIELD_VARIANT = 'filled' | 'outlined';
 
-@customElement({ name: 'moo-text-field-test', template, containerless: true, capture: true })
+@customElement({ name: 'moo-text-field', template, containerless: true, capture: true })
 export class MooTextField implements ICustomElementViewModel {
     @bindable({ mode: BindingMode.twoWay }) value: string | number | undefined | null;
     @bindable variant: MOO_TEXT_FIELD_VARIANT = 'outlined';
