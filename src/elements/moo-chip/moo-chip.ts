2b377473aadcab6d24b2a5314dae1ef067aef810
Collin Herber <rustaddicts@gmail.com>
2023-11-28 10:47:51 -0500

    fix: chip default and rendered chips

--- src/elements/moo-chip/moo-chip.ts
+++ src/elements/moo-chip/moo-chip.ts
@@ -4,5 +4,5 @@ export type MOO_CHIP_SET_VARIANT = 'assist' | 'filter' | 'input' | 'suggestion';
 import template from './moo-chip.html?raw';
 @customElement({name:'moo-chip', template, containerless: true, capture: true})
 export class MooChip implements ICustomElementViewModel {
-    @bindable variant: MOO_CHIP_SET_VARIANT = 'assist';
+    @bindable variant: MOO_CHIP_SET_VARIANT = 'input';
 }
