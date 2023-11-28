19317328679952b3c5233d8103789f64fc109c63
Collin Herber <rustaddicts@gmail.com>
2023-11-28 08:44:53 -0500

    feat: add attribute spreading to moo-switch

--- src/elements/moo-switch/moo-switch.ts
+++ src/elements/moo-switch/moo-switch.ts
@@ -3,7 +3,7 @@ import '@material/web/switch/switch';
 import { MdSwitch } from '@material/web/all';
 import { bindable, BindingMode, customElement, ICustomElementViewModel } from "@aurelia/runtime-html";
 import template from './moo-switch.html?raw';
-@customElement({name:'moo-switch', template, containerless: true})
+@customElement({name:'moo-switch', template, containerless: true, capture: true})
 export class MooSwitch implements ICustomElementViewModel {
     @bindable({ mode: BindingMode.twoWay }) value: boolean;
     @bindable class: string;
