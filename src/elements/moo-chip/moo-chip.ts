f9737ffbc3b7a21d3b1d8087d330d33f8615d7ec
Collin Herber <rustaddicts@gmail.com>
2023-11-28 11:09:29 -0500

    feat: add remove event to moo chip

--- src/elements/moo-chip/moo-chip.ts
+++ src/elements/moo-chip/moo-chip.ts
@@ -1,8 +1,23 @@
-import {bindable, customElement, ICustomElementViewModel} from "@aurelia/runtime-html";
+import { bindable, customElement, ICustomElementViewModel } from '@aurelia/runtime-html';
 
 export type MOO_CHIP_SET_VARIANT = 'assist' | 'filter' | 'input' | 'suggestion';
 import template from './moo-chip.html?raw';
-@customElement({name:'moo-chip', template, containerless: true, capture: true})
+
+import { MdAssistChip, MdFilterChip, MdInputChip, MdSuggestionChip } from '@material/web/all';
+
+@customElement({ name: 'moo-chip', template, containerless: true, capture: true })
 export class MooChip implements ICustomElementViewModel {
     @bindable variant: MOO_CHIP_SET_VARIANT = 'input';
+    @bindable remove: () => void;
+    chipEl: MdAssistChip | MdInputChip | MdSuggestionChip | MdFilterChip;
+
+    attached() {
+        this.chipEl.addEventListener('remove', () => this.handleRemove());
+    }
+
+    handleRemove() {
+        if (this.remove) {
+            this.remove();
+        }
+    }
 }
