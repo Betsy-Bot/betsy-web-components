6b8b79ac120c5f3dbd4cbd4e0ff83e0d1cf3c3e8
CollinHerber <rustaddicts@gmail.com>
2024-01-14 11:21:08 -0500

    feat: add capture to moo menu item

--- src/elements/moo-menu-item/moo-menu-item.ts
+++ src/elements/moo-menu-item/moo-menu-item.ts
@@ -2,7 +2,7 @@ import { customElement, ICustomElementViewModel } from '@aurelia/runtime-html';
 
 import template from './moo-menu-item.html?raw';
 
-@customElement({ name:'moo-menu-item', template, containerless: true })
+@customElement({ name:'moo-menu-item', template, containerless: true, capture: true })
 export class MooMenuItem implements ICustomElementViewModel {
 
 }
