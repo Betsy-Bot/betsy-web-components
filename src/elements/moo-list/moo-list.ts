252fa0457beae88801bed5ec9d577d3a1caa2255
CollinHerber <rustaddicts@gmail.com>
2024-01-15 14:55:26 -0500

    feat: add capture to moo list

--- src/elements/moo-list/moo-list.ts
+++ src/elements/moo-list/moo-list.ts
@@ -2,6 +2,6 @@ import { customElement, ICustomElementViewModel } from '@aurelia/runtime-html';
 
 import template from './moo-list.html?raw';
 
-@customElement({ name:'moo-list', template, containerless: true })
+@customElement({ name:'moo-list', template, containerless: true, capture: true })
 export class MooList implements ICustomElementViewModel {
 }
