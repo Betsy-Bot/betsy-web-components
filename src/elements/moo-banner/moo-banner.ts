45d23375570ad1935f9e6afee6fa83fabcbdf037
Collin Herber <rustaddicts@gmail.com>
2024-05-13 13:26:52 -0400

    fix: types on banner

--- src/elements/moo-banner/moo-banner.ts
+++ src/elements/moo-banner/moo-banner.ts
@@ -1,14 +1,14 @@
 import './moo-banner.scss';
 
 import { CloseReason, MDCBanner } from '@material/banner';
-import {bindable, BindingMode, customElement, ICustomElementViewModel} from "@aurelia/runtime-html";
+import {bindable, BindingMode, customElement, ICustomElementViewModel, watch} from "@aurelia/runtime-html";
 
 import template from './moo-banner.html?raw';
 @customElement({name:'moo-banner', template, containerless: true, capture: true})
 export class MooBanner implements ICustomElementViewModel {
-    @bindable primaryAction;
-    @bindable secondaryAction;
-    @bindable({ mode: BindingMode.twoWay }) open;
+    @bindable primaryAction: string;
+    @bindable secondaryAction: string;
+    @bindable({ mode: BindingMode.twoWay }) open: boolean;
     bannerEl: HTMLButtonElement;
     banner: MDCBanner;
 
@@ -19,12 +19,12 @@ export class MooBanner implements ICustomElementViewModel {
         }
     }
 
+    @watch('open')
     openChanged() {
         if (this.open) {
             this.banner.open();
         } else {
             this.banner.close(CloseReason.PRIMARY);
         }
-
     }
 }
